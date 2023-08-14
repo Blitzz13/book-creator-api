9fcd0662ec37ea8743395669c7cf5bd90c64261f
Blitzz13 <thebeardman123@gmail.com>
2023-08-14 21:19:10 +0300

    Implement book rating

---
+++ models/ratingModel.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ratingSchema = new mongoose.Schema({
+    user: { type: Schema.Types.ObjectId, ref: 'User' },
+    book: { type: Schema.Types.ObjectId, ref: 'Book' },
+    rating: { type: Number, min: 1, max: 5 },
+    comment: String,
+}, { timestamps: true });
+
+module.exports = mongoose.model("Rating", ratingSchema);
